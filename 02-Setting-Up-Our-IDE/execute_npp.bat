1913555cacfebdb0d1300ad5c4ee299d40913e6f
Tamil Programmer <139563085+tamilcpp-programmer@users.noreply.github.com>
2023-09-03 12:52:20 +0530

    Update execute_npp.bat

--- 02-Setting-Up-Our-IDE/execute_npp.bat
+++ 02-Setting-Up-Our-IDE/execute_npp.bat
@@ -16,37 +16,35 @@ REM echo File Name with Extension: "%FILENAME_EXT%"
 REM Change Directory
 cd "%DIRNAME%"
 
-echo ++++++++++++++++++++++++++++++++++++++++
-echo Executing  %FILENAME_EXT%  ...
-echo ++++++++++++++++++++++++++++++++++++++++
+echo ===============================================
+echo Executing  %FILENAME%.exe  ...
+echo ===============================================
 
-set CC_BIN=gcc
 set CPP_BIN=g++
 
-set CC_EXT=.c
 set CPP_EXT1=.c++
 set CPP_EXT2=.cpp
 set CPP_EXT3=.cxx
 
-
 IF /I %FILE_EXT% == %CPP_EXT1% GOTO __CPPEXEC
 IF /I %FILE_EXT% == %CPP_EXT2% GOTO __CPPEXEC
 IF /I %FILE_EXT% == %CPP_EXT3% GOTO __CPPEXEC
-IF /I %FILE_EXT% == %CC_EXT%  GOTO __CEXEC
-
-GOTO END
-
-:__CEXEC
-call %FILENAME%
 GOTO END
 
 :__CPPEXEC
+REM Execute C++ Programs
+REM Check if file exists
+if exist "%FILENAME%.exe" (
+    call "%FILENAME%.exe"
+) else (
+    echo %FILENAME%.exe : Executable file not found!!! Please compile before executing...
+)
 
-call %FILENAME%
 GOTO END
 
 :END
 echo.
-echo ++++++++++++++++++++++++++++++++++++++++
+echo ===============================================
 echo Execution - Completed !!!
 pause
+exit 0
