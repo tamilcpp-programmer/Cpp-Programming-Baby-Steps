c8fa7c6e4112ca011dc010c375d6da93cbebfcb5
Tamil Programmer <139563085+tamilcpp-programmer@users.noreply.github.com>
2023-09-03 12:39:57 +0530

    Update compile_npp.bat

--- 02-Setting-Up-Our-IDE/compile_npp.bat
+++ 02-Setting-Up-Our-IDE/compile_npp.bat
@@ -4,24 +4,75 @@ set FULL_FILENAME=%~1
 set FILENAME=%~n1
 set DIRNAME=%~dp1
 set FILE_EXT=%~x1
-set FILENAME_EXT="%FILENAME%%FILE_EXT%"
+set FILENAME_EXT=%FILENAME%%FILE_EXT%
+
 
 REM DEBUG
-REM echo Full File Name: "%FULL_FILENAME%"
-REM echo File Name: "%FILENAME%"
-REM echo File Directory: "%DIRNAME%"
-REM echo File Extension: "%FILE_EXT%"
-REM echo File Name with Extension: "%FILENAME_EXT%"
+echo Full File Name: "%FULL_FILENAME%"
+echo File Name: "%FILENAME%"
+echo File Directory: "%DIRNAME%"
+echo File Extension: "%FILE_EXT%"
+echo File Name with Extension: "%FILENAME_EXT%"
 
-set CC_BIN=gcc
+set CPP_BIN=g++@echo off
+
+REM Parse the command-line arguments
+set FULL_FILENAME=%~1
+set FILENAME=%~n1
+set DIRNAME=%~dp1
+set FILE_EXT=%~x1
+set FILENAME_EXT=%FILENAME%%FILE_EXT%
+
+REM DEBUG
+echo Full File Name: "%FULL_FILENAME%"
+echo File Name: "%FILENAME%"
+echo File Directory: "%DIRNAME%"
+echo File Extension: "%FILE_EXT%"
+echo File Name with Extension: "%FILENAME_EXT%"
+
+REM Compilers
 set CPP_BIN=g++
 
-set CC_EXT=.c
+REM File Extensions
+REM C++ File Extensions
 set CPP_EXT1=.c++
 set CPP_EXT2=.cpp
 set CPP_EXT3=.cxx
 
-set CPP_OPTIONS=-std=c++20
+REM set CPP_OPTIONS=-std=c++11
+set CPP_OPTIONS=
+set LIB_FLAGS=
+set INCLUDE_DIR=
+set LIB_DIR=
+
+REM Change Directory to the source file's directory
+cd "%DIRNAME%"
+
+REM branch based on file extensions
+IF /I %FILE_EXT% == %CPP_EXT1% GOTO __CPPCOMPILE
+IF /I %FILE_EXT% == %CPP_EXT2% GOTO __CPPCOMPILE
+IF /I %FILE_EXT% == %CPP_EXT3% GOTO __CPPCOMPILE
+GOTO END
+
+:__CPPCOMPILE
+echo --- Compiling C++ Program ... ---
+set CPP_OPTIONS=-Wall -std=c++20
+echo "Compiling : %CPP_BIN% %INCLUDE_DIR% %FILENAME_EXT% %CPP_OPTIONS%  -o %FILENAME%.exe %LIB_DIR% %LIB_FLAGS%"
+call %CPP_BIN% %INCLUDE_DIR% %FILENAME_EXT% %CPP_OPTIONS%  -o %FILENAME%.exe %LIB_DIR% %LIB_FLAGS% & IF ERRORLEVEL 1 (echo. && echo "ERROR - Compilation Error - Please Fix !!!" ) ELSE echo "Compilation success !!!"
+
+GOTO END
+
+:END
+pause
+exit /b %ERRORLEVEL%
+
+
+
+set CPP_EXT1=.c++
+set CPP_EXT2=.cpp
+set CPP_EXT3=.cxx
+
+REM set CPP_OPTIONS=-std=c++11
 set CPP_OPTIONS=
 set LIB_FLAGS=
 set INCLUDE_DIR=
@@ -30,54 +81,23 @@ set LIB_DIR=
 REM Change Directory
 cd "%DIRNAME%"
 
-IF /I %FILE_EXT% == %CC_EXT%  GOTO __CCOMPILE
 IF /I %FILE_EXT% == %CPP_EXT1% GOTO __CPPCOMPILE
 IF /I %FILE_EXT% == %CPP_EXT2% GOTO __CPPCOMPILE
 IF /I %FILE_EXT% == %CPP_EXT3% GOTO __CPPCOMPILE
-
 GOTO END
 
-:__CCOMPILE
-echo --- Compiling C Program ... ---
-
-REM -- Check for thread usage
-findstr "thread" %FILENAME_EXT% >NUL
-if ERRORLEVEL 0 set LIB_FLAGS=%LIB_FLAGS% -lpthread
-
-call %CC_BIN% %FILENAME_EXT%  -o %FILENAME%.exe %LIB_FLAGS% & IF ERRORLEVEL 1 (echo. && echo "ERROR - Compilation Error - Please Fix !!!" ) ELSE echo "Compilation success !!!"
-GOTO END
 
 :__CPPCOMPILE
 echo --- Compiling C++ Program ... ---
 
-REM -- Check for thread usage
-findstr /I "thread" %FILENAME_EXT% >NUL 2>&1
-if ERRORLEVEL 1 (
-    REM echo "No Threading..."
-) else (
-    set LIB_FLAGS=%LIB_FLAGS% -lpthread
-)
+set CPP_OPTIONS=-Wall -std=c++20
 
-findstr /I "c++11" %FILENAME_EXT% >NUL 2>&1
-if ERRORLEVEL 1 (
-    REM "C++ Program..."
-) else (
-    set CPP_OPTIONS=-std=c++11
-)
+echo "Compiling : %CPP_BIN% %INCLUDE_DIR% %FILENAME_EXT% %CPP_OPTIONS%  -o %FILENAME%.exe %LIB_DIR% %LIB_FLAGS%"
 
-findstr /I "c++17" %FILENAME_EXT% >NUL 2>&1
-if ERRORLEVEL 1 (
-    REM "C++ Program..."
-) else (
-    set CPP_OPTIONS=-std=c++17
-)
-
-echo "Compiling: %CPP_BIN% %INCLUDE_DIR% %FILENAME_EXT% -o %FILENAME%.exe %CPP_OPTIONS% %LIB_DIR% %LIB_FLAGS%"
-
-call %CPP_BIN% %INCLUDE_DIR% %FILENAME_EXT% -o %FILENAME%.exe %CPP_OPTIONS% %LIB_DIR% %LIB_FLAGS% & IF ERRORLEVEL 1 (echo. && echo "ERROR - Compilation Error - Please Fix !!!" ) ELSE echo "Compilation success !!!"
+call %CPP_BIN% %INCLUDE_DIR% %FILENAME_EXT% %CPP_OPTIONS%  -o %FILENAME%.exe %LIB_DIR% %LIB_FLAGS% & IF ERRORLEVEL 1 (echo. && echo "ERROR - Compilation Error - Please Fix !!!" ) ELSE echo "Compilation success !!!"
 
 GOTO END
 
 :END
 pause
-exit /b 0
+exit /b %ERRORLEVEL%
