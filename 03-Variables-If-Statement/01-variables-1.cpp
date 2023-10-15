2ced72a204f1df4c297b5558dd1ad48c8b78479c
Tamil Programmer <139563085+tamilcpp-programmer@users.noreply.github.com>
2023-10-15 18:49:45 +0530

    Create 01-variables-1.cpp

---
+++ 03-Variables-If-Statement/01-variables-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+    
+    std::cout << "a = " << a << std::endl;
+    a = 42;
+    std::cout << "a = " << a << std::endl;
+    a = 181;
+    std::cout << "a = " << a << std::endl;
+    return 0;
+}
